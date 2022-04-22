c84599078ce0130507abc26103edcd0bf1e3688f
BlackSmithSouL <kowalskimarcinkontakt@gmail.com>
2022-04-22 19:07:01 +0200

    Instal styled-components absolute paths and create footer and header components

---
+++ src/app/lib/locale/en_GB.ts
@@ -0,0 +1,16 @@
+import { Dictionary } from "../types"
+
+export const en_GB: Dictionary = {
+    companyName: 'CodeMaskAcademy',
+    components: {
+        header: {
+            github: 'Github',
+            discord: 'Discord',
+            title: 'Translator ReactJS'
+        },
+        footer: {
+            flatIcons: 'FlatIcons',
+            libreTranslate: 'LibreTranslate'
+        }
+    }
+}
---
+++ src/app/lib/types/theme.ts
@@ -0,0 +1,10 @@
+export type AppTheme = {
+    colors: {
+        primart: string,
+        background: string,
+        foreground: string,
+        error: string,
+        typography: string,
+        input: string
+    }
+}
